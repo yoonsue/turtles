4681c94143ce3d9b10900170ddc734b4a2d2bdfd
yoonsue <suejoe1996@gmail.com>
2018-12-10 22:39:51 +0900

    main: implement the newVerticesOfHexGenerator (number of vertex list to increase at the time each hexagonal is put)

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 )
 
 func main() {
-	sizeList := []int{
-		6,
-		4, 3, 3, 3, 3, 2,
-		4, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 1,
-		4, 2, 2, 3, 2, 2, 3, 2, 2, 3, 2, 2, 3, 2, 2, 3, 2, 1,
-		4, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 1,
-	}
 
 	fmt.Printf("Turtles Problem Sloving with GA\n")
 
@@ -21,14 +14,19 @@ func main() {
 	fmt.Printf("How many hexagons you want? ")
 	fmt.Scanf("%d", &n)
 
+	listOfNumOfVerticesToAdd := newVerticesOfHexGenerator(n)
+
 	i := 0
 	for i < n {
-		endInt += sizeList[i]
+		endInt += listOfNumOfVerticesToAdd[i]
 		i++
 	}
 	myrand := random(endInt)
 
-	fmt.Println("size: ", n, "( ~ ", endInt, ")\nlist: ", myrand)
+	randomRoundCheck := roundCheck(n)
+
+	fmt.Println("size of List: ", len(listOfNumOfVerticesToAdd), "listOfNumOfVerticesToAdd: ", listOfNumOfVerticesToAdd)
+	fmt.Println("round #", randomRoundCheck, "size: ", n, "( ~ ", endInt, ")\nlist: ", myrand)
 }
 
 func random(size int) []int {
@@ -43,4 +41,101 @@ func random(size int) []int {
 		randList[v] = list[i]
 	}
 	return randList
+}
+
+func roundCheck(currentHexNum int) int {
+	an := 1
+	roundNum := 1
+
+	for {
+		lastHexOfCurrentRound := an + (roundNum * (6 * (roundNum - 1)) / 2)
+		// fmt.Println("last Hex of Round #", roundNum, " is Hex #", lastHexOfCurrentRound)
+
+		if currentHexNum > lastHexOfCurrentRound {
+			roundNum++
+		} else {
+			break
+		}
+	}
+	return roundNum
+}
+
+func newVerticesOfHexGenerator(hexN int) []int {
+	var listOfNumOfVerticesToAdd []int
+
+	currentHexNum := 1
+
+	an := 1
+	roundNum := 1
+	lastHexOfCurrentRound := an + (roundNum * (6 * (roundNum - 1)) / 2)
+
+	numOfVerticesToAdd := 0
+
+	for currentHexNum <= hexN {
+		tmpBetweenTwoVertexOfHex := roundNum - 2
+
+		if currentHexNum == lastHexOfCurrentRound+1 { // first vertex for current ROUND
+			numOfVerticesToAdd = 4
+			roundNum++
+			lastHexOfCurrentRound = an + (roundNum * (6 * (roundNum - 1)) / 2)
+		} else if currentHexNum == lastHexOfCurrentRound { // last vertex for current ROUND
+			if roundNum == 1 { // ROUND1 last vertex
+				numOfVerticesToAdd = 6
+			} else if roundNum == 2 { // ROUND2 last vertex
+				numOfVerticesToAdd = 2
+			} else { // last vertex (more than 2 ROUND)
+				numOfVerticesToAdd = 1
+			}
+		} else {
+			for tmpBetweenTwoVertexOfHex > 0 {
+				numOfVerticesToAdd = 2
+				listOfNumOfVerticesToAdd = append(listOfNumOfVerticesToAdd, numOfVerticesToAdd)
+				fmt.Println("Round #", roundNum, " Hex#", currentHexNum, "lastHex#", lastHexOfCurrentRound, "numOfVerticesToAdd: ", numOfVerticesToAdd)
+				tmpBetweenTwoVertexOfHex--
+				currentHexNum++
+				if currentHexNum == lastHexOfCurrentRound {
+					break
+				}
+			}
+			if currentHexNum != lastHexOfCurrentRound {
+				numOfVerticesToAdd = 3
+			} else {
+				numOfVerticesToAdd = 1
+			}
+			fmt.Println("Round #", roundNum, " Hex#", currentHexNum, "lastHex#", lastHexOfCurrentRound, "numOfVerticesToAdd: ", numOfVerticesToAdd)
+		}
+		fmt.Println("Round #", roundNum, " Hex#", currentHexNum, "lastHex#", lastHexOfCurrentRound, "numOfVerticesToAdd: ", numOfVerticesToAdd)
+		listOfNumOfVerticesToAdd = append(listOfNumOfVerticesToAdd, numOfVerticesToAdd)
+
+		currentHexNum++
+	}
+	return listOfNumOfVerticesToAdd
+}
+
+func sumOfVerticesInHex(randList []int, currentHexNum int, incrementOfInt int) int {
+
+	tmpIntList := make([]int, 0, 6)
+
+	sum := 0
+
+	if currentHexNum == 1 { // Round 1.
+		tmpIntList = randList[:6]
+	} else { // From Round 2.
+		if incrementOfInt == 4 { // Round start-point
+			tmpIntList = randList
+		} else if incrementOfInt == 3 { // 6 vertices
+
+		} else if incrementOfInt == 2 { // rest of edges
+
+		} else if incrementOfInt == 1 { // Round end-point (Start at Round 3)
+
+		}
+	}
+
+	for i, _ := range tmpIntList {
+		sum += tmpIntList[i]
+	}
+
+	return sum
+
 }
